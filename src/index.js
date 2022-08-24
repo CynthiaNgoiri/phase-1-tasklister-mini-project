53ac83bf4a22d1850a28bde9cd17ecc41c82fb9c
Cynthia Njomo <cynthiangoirinjomo@gmail.com>
2022-08-24 12:48:26 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildToDoList(e.target.formIdHere.value)
+    form.reset()
+  
+  })
 });
+
+function buildToDoList(todo){
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  let btn1=document.createElement('select')
+  btn.addEventListener('click', deleteHandler)
+  btn.textContent = 'x'
+  li.textContent = `${todo} `
+  li.appendChild(btn)
+  console.log(li)
+  document.querySelector('#tasks').appendChild(li)
+}
+function deleteHandler(e){
+  e.target.parentNode.remove()
+}
+
+function sortToDoList(e){
+  e.target.parentNode.sort()
+}
